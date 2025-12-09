b2bbb301424b89499ecc96cbb8fe4e1a4309431c
Abdellah Lambaraa <abdellah17lambaraa@gmail.com>
2025-12-09 19:48:36 +0100

    Architecture Micro-services

---
+++ democustomer-service/src/main/java/org/example/democustomerservice/DemocustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.democustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocustomerServiceApplication.class, args);
+	}
+
+}
---
+++ democustomer-service/src/test/java/org/example/democustomerservice/DemocustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.democustomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
